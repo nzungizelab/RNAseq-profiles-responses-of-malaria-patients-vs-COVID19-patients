e3bcfc5b6a0078280d6ed6be00d37924eb8e87d6
Nzungize Lambert <nzulapa@gmailcom>
2021-12-15 09:38:33 +0200

    heatmap

--- cov_mal.R
+++ cov_mal.R
@@ -154,3 +154,48 @@ head(y_filtered)
 
 dim(y_filtered) #Checking after filtering step
 #[1] 31154    13
+
+
+
+## Heatmap for Tab3_overlap
+
+library(gplots)
+library(RColorBrewer)
+
+#Reading in data and transform it into matrix format
+data <- read.csv("cov_mal.de_logFC.csv", comment.char="#")
+rnames <- data[,1]                            # assign labels in column 1 to "rnames"
+cov_mal_data <- data.matrix(data[,2:ncol(data)])  # transform column 2-3 into a matrix
+rownames(cov_mal_data) <- rnames                  # assign row names
+
+#Customizing and plotting the heat map
+
+# creates a own color palette from red to green
+my_palette <- colorRampPalette(c("red", "yellow", "green"))(n = 61)
+
+# (optional) defines the color breaks manually for a "skewed" color transition
+col_breaks = c(seq(-1,0,length=100),  # for red
+               seq(0.01,0.8,length=100),           # for yellow
+               seq(0.81,1,length=100))             # for green
+
+# creates a 5 x 5 inch image
+png(paste0(outpath,"heatmap.png"), # create and save PNG for the heat map 
+    width = 5*300,        # 5 x 300 pixels
+    height = 5*300,
+    res = 300,            # 300 pixels per inch
+    pointsize = 8)        # smaller font size
+
+heatmap.2(cov_mal_data,
+          #cellnote = cov_mal_data,  # same data set for cell labels
+          main = "Overlapped genes expressed", # heat map title
+          notecol = "black",      # change font color of cell labels to black
+          density.info="none",  # turns off density plot inside color legend
+          trace="none",         # turns off trace lines inside the heat map
+          margins = c(12,10),     # widens margins around plot
+          col=my_palette,       # use on color palette defined earlier
+          #breaks=col_breaks,    # enable color transition at specified limits
+          dendrogram= "row",     # only draw a row dendrogram
+          Colv= "NA")            # turn off column clustering
+
+dev.off()               # close the PNG device
+
